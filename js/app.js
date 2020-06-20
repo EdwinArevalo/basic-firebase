031981c91a8e7aa13165348fba1f1779154ec973
EdwinArevalo <edwin.arevalo@tecsup.edu.pe>
2020-06-20 15:59:41 -0500

    previous

--- js/app.js
+++ js/app.js
@@ -12,14 +12,48 @@ const saveTask = (title, description) => {
 
 const getTasks = () => db.collection('tasks').get();
 
-window.addEventListener('DOMContentLoaded', async (e) => {
-    const querySnapshot = await getTasks();
-    
-    querySnapshot.forEach(doc => {
-        console.log(doc.data());
-        ta
-    });
+const getTask = id => db.collection('tasks').doc(id).get();
 
+const onGetTask = (callback) => db.collection('tasks').onSnapshot(callback);
+
+const deleteTask = id => db.collection('tasks').doc(id).delete();
+
+window.addEventListener('DOMContentLoaded', async (e) => {  
+    await onGetTask((querySnapshot)=> {
+        taskContainer.innerHTML = '';
+        querySnapshot.forEach(doc => {
+            const task = doc.data();
+            task.id = doc.id;
+            //console.log(task);
+    
+            taskContainer.innerHTML+= `<div class="card card-body mt-2">
+                <h3 class="h5">
+                ${task.title[0].toUpperCase()+task.title.slice(1)}
+                </h3>
+                <p>${task.description}</p>
+                <div>
+                <button class="btn btn-primary btn-delete" data-id="${task.id}">Delete</button>
+                <button class="btn btn-secondary btn-edit" data-id="${task.id}">Edit</button>
+                </div>
+            </div>`; 
+
+            const btnsDelete = document.querySelectorAll('.btn-delete');
+            btnsDelete.forEach( btn => {
+                btn.addEventListener('click',async (e) => {
+                    await deleteTask(e.target.dataset.id);
+                });
+            });
+
+            const btnsEdit = document.querySelectorAll('.btn-edit');
+            btnsEdit.forEach( btn => {
+                btn.addEventListener('click',async (e) => {
+                    const task = await getTask(e.target.dataset.id);
+                    console.log(task.data());
+                });
+            });
+            
+        });
+    });
 });
 
 taskForm.addEventListener('submit',async (e) =>{
@@ -30,6 +64,8 @@ taskForm.addEventListener('submit',async (e) =>{
 
     await saveTask(title.value,description.value);
     
+
+
     taskForm.reset();
     title.focus();
  
